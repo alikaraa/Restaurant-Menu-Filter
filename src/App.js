ddb73145e0f8e78b71f5f6d27a7abe72468ccebc
Ali Kara <56256760+alikaraa@users.noreply.github.com>
2021-03-07 01:15:08 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const allCategories = ['all', ...new Set(items.map((item) => item.category))];
 
 function App() {
   const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState(allCategories);
+  const [categories] = useState(allCategories);
 
   const filterItems = (category) => {
     if (category === 'all') {
